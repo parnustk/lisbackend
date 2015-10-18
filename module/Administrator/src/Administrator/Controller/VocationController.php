9282bf79e09a1bad8ece25647f53ff991dd1cd51
Juhan Kõks <juhankoks@gmail.com>
2015-10-18 14:51:38 +0300

    VocationController.php

--- module/Administrator/src/Administrator/Controller/VocationController.php
+++ module/Administrator/src/Administrator/Controller/VocationController.php
@@ -3,16 +3,40 @@
 namespace Administrator\Controller;
 
 use Zend\View\Model\JsonModel;
-use Exception;
-use Zend\Json\Json;
 use Core\Controller\AbstractBaseController;
 
-class VocationController extends AbstractBaseController
-{
+/**
+ * @author Juhan
+ */
+class VocationController extends AbstractBaseController {
 
-    public function indexAction()
-    {
-        return new ViewModel();
+    /**
+     * POST
+     * 
+     * @param type $data
+     * @return JsonModel
+     */
+    public function create($data) {
+        return new JsonModel(
+                $this
+                        ->getServiceLocator()
+                        ->get('vocation')
+                        ->Create($data)
+        );
+    }
+
+    /**
+     * GET
+     *
+     * @return JsonModel
+     */
+    public function getList() {
+        return new JsonModel($this
+                        ->getServiceLocator()
+                        ->get('vocation')
+                        ->GetList()
+        );
     }
 
 }
+
