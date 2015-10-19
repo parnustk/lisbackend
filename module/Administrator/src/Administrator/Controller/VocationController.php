18f49e3f352657c79fa438c6cda9c07302c05766
Juhan Kõks <juhankoks@gmail.com>
2015-10-19 14:38:00 +0300

    Implement Administrator\Controller\VocationController and test class #8 Controller improved again

--- module/Administrator/src/Administrator/Controller/VocationController.php
+++ module/Administrator/src/Administrator/Controller/VocationController.php
@@ -26,6 +26,37 @@ class VocationController extends AbstractBaseController {
     }
 
     /**
+     * DELETE
+     * 
+     * @param type $id
+     * @param type $data
+     * @return JsonModel
+     */
+    public function delete($id) {
+        return new JsonModel(
+                $this
+                        ->getServiceLocator()
+                        ->get('vocation_service')
+                        ->Delete($id)
+        );
+    }
+
+    /**
+     * GET
+     * 
+     * @param type $id
+     * @return JsonModel
+     */
+    public function get($id) {
+        return new JsonModel(
+                $this
+                        ->getServiceLocator()
+                        ->get('vocation_service')
+                        ->Get($id)
+        );
+    }
+
+    /**
      * GET
      *
      * @return JsonModel
@@ -38,5 +69,20 @@ class VocationController extends AbstractBaseController {
         );
     }
 
-}
+    /**
+     * PUT
+     * 
+     * @param type $id
+     * @param type $data
+     * @return JsonModel
+     */
+    public function update($id, $data) {
+        return new JsonModel(
+                $this
+                        ->getServiceLocator()
+                        ->get('vocation_service')
+                        ->Update($id, $data)
+        );
+    }
 
+}
