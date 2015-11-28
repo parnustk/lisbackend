e0f0017559d1448d7564e31840deff2c3a0e2d4e
Sander Mets <sandermets0gmail.com>
2015-11-28 17:05:04 +0200

    Implement group controller for administrator #16 started test, controller, service

---
+++ module/Core/src/Core/Service/GroupService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core\Service;
+
+use Exception;
+
+class GroupService extends AbstractBaseService
+{
+
+    /**
+     * 
+     * @param array $data
+     * @return type
+     */
+    public function Create(array $data)
+    {
+        return [
+            'success' => true,
+            $data
+        ];
+    }
+
+}
