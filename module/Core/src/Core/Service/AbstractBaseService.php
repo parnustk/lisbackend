bce2485414236d63c38236efa9fbf11c85c3996e
Sander Mets <sandermets0gmail.com>
2016-01-17 20:45:54 +0200

    #25 refactoring service

--- module/Core/src/Core/Service/AbstractBaseService.php
+++ module/Core/src/Core/Service/AbstractBaseService.php
@@ -5,6 +5,7 @@ namespace Core\Service;
 use Zend\ServiceManager\ServiceManagerAwareInterface;
 use Zend\ServiceManager\ServiceManager;
 use Doctrine\ORM\EntityManager;
+use Exception;
 
 /**
  * Teting Service set up. Remove later on.
@@ -63,6 +64,142 @@ abstract class AbstractBaseService implements ServiceManagerAwareInterface
     {
         $this->entityManager = $entityManager;
         return $this;
+    }
+
+    /**
+     * 
+     * @param array $data
+     * @param stdClass|NULL $extra
+     * @return array
+     */
+    public function Create($data, $extra = null)
+    {
+        try {
+            return [
+                'success' => true,
+                'data' => $this
+                        ->getEntityManager()
+                        ->getRepository($this->baseEntity)
+                        ->Create($data, true, $extra)
+            ];
+        } catch (Exception $ex) {
+
+            return [
+                'success' => false,
+                'message' => $ex->getMessage()
+            ];
+        }
+    }
+
+    /**
+     * 
+     * @param array $params
+     * @param stdClass|NULL $extra
+     * @return array
+     */
+    public function GetList($params, $extra = null)
+    {
+        try {
+
+            $p = $this->getEntityManager()
+                    ->getRepository($this->baseEntity)
+                    ->GetList($params, $extra);
+
+            $p->setItemCountPerPage($params['limit']);
+            $p->setCurrentPageNumber($params['page']);
+
+
+            $params['itemCount'] = $p->getTotalItemCount();
+            $params['pageCount'] = $p->count();
+
+            return [
+                'success' => true,
+                'params' => $params,
+                'data' => (array) $p->getCurrentItems(),
+            ];
+        } catch (Exception $ex) {
+
+            return [
+                'success' => false,
+                'message' => $ex->getMessage()
+            ];
+        }
+    }
+
+    /**
+     * 
+     * @param int|string $id
+     * @param stdClass|NULL $extra
+     * @return array
+     */
+    public function Get($id, $extra = null)
+    {
+        $Id = (int) $id;
+        try {
+            return [
+                'success' => true,
+                'data' => $this
+                        ->getEntityManager()
+                        ->getRepository($this->baseEntity)
+                        ->Get($Id, true, $extra)
+            ];
+        } catch (Exception $ex) {
+            return [
+                'success' => false,
+                'message' => $ex->getMessage()
+            ];
+        }
+    }
+
+    /**
+     * 
+     * @param int|string $id
+     * @param array $data
+     * @param stdClass|NULL $extra
+     * @return array
+     */
+    public function Update($id, $data, $extra = null)
+    {
+        $Id = (int) $id;
+        try {
+            return [
+                'success' => true,
+                'data' => $this
+                        ->getEntityManager()
+                        ->getRepository($this->baseEntity)
+                        ->Update($Id, $data, true, $extra)
+            ];
+        } catch (Exception $ex) {
+            return [
+                'success' => false,
+                'message' => $ex->getMessage()
+            ];
+        }
+    }
+
+    /**
+     * 
+     * @param int|string $id
+     * @param stdClass|NULL $extra
+     * @return array
+     */
+    public function Delete($id, $extra = null)
+    {
+        $Id = (int) $id;
+        try {
+            return [
+                'success' => true,
+                'id' => $this
+                        ->getEntityManager()
+                        ->getRepository($this->baseEntity)
+                        ->Delete($Id, $extra)
+            ];
+        } catch (Exception $ex) {
+            return [
+                'success' => false,
+                'message' => $ex->getMessage()
+            ];
+        }
     }
 
 }
--- module/Core/src/Core/Service/VocationService.php
+++ module/Core/src/Core/Service/VocationService.php
@@ -5,12 +5,10 @@
  * 
  * @link      https://github.com/parnustk/lisbackend
  * @copyright Copyright (c) 2016 Lis Team
- * @license   https://opensource.org/licenses/MIT MIT License
+ * @license   TODO
  */
 
 namespace Core\Service;
-
-use Exception;
 
 /**
  * @author Juhan, Sander Mets <sandermets0@gmail.com>
@@ -19,139 +17,9 @@ class VocationService extends AbstractBaseService
 {
 
     /**
-     * 
-     * @param array $params
-     * @param stdClass|NULL $extra
-     * @return array
+     *
+     * @var string 
      */
-    public function GetList($params, $extra = null)
-    {
-        try {
-
-            $p = $this->getEntityManager()
-                    ->getRepository('Core\Entity\Vocation')
-                    ->GetList($params, $extra);
-
-            $p->setItemCountPerPage($params['limit']);
-            $p->setCurrentPageNumber($params['page']);
-            
-            
-            $params['itemCount'] = $p->getTotalItemCount();
-            $params['pageCount'] = $p->count();
-
-            return [
-                'success' => true,
-                'params' => $params,
-                'data' => (array) $p->getCurrentItems(),
-            ];
-        } catch (Exception $ex) {
-
-            return [
-                'success' => false,
-                'message' => $ex->getMessage()
-            ];
-        }
-    }
-
-    /**
-     * 
-     * @param int|string $id
-     * @param stdClass|NULL $extra
-     * @return array
-     */
-    public function Get($id, $extra = null)
-    {
-        $Id = (int) $id;
-        try {
-            return [
-                'success' => true,
-                'data' => $this
-                        ->getEntityManager()
-                        ->getRepository('Core\Entity\Vocation')
-                        ->Get($Id, true, $extra)
-            ];
-        } catch (Exception $ex) {
-            return [
-                'success' => false,
-                'message' => $ex->getMessage()
-            ];
-        }
-    }
-
-    /**
-     * 
-     * @param array $data
-     * @param stdClass|NULL $extra
-     * @return array
-     */
-    public function Create($data, $extra = null)
-    {
-        try {
-            return [
-                'success' => true,
-                'data' => $this
-                        ->getEntityManager()
-                        ->getRepository('Core\Entity\Vocation')
-                        ->Create($data, true, $extra)
-            ];
-        } catch (Exception $ex) {
-
-            return [
-                'success' => false,
-                'message' => $ex->getMessage()
-            ];
-        }
-    }
-
-    /**
-     * 
-     * @param int|string $id
-     * @param array $data
-     * @param stdClass|NULL $extra
-     * @return array
-     */
-    public function Update($id, $data, $extra = null)
-    {
-        $Id = (int) $id;
-        try {
-            return [
-                'success' => true,
-                'data' => $this
-                        ->getEntityManager()
-                        ->getRepository('Core\Entity\Vocation')
-                        ->Update($Id, $data, true, $extra)
-            ];
-        } catch (Exception $ex) {
-            return [
-                'success' => false,
-                'message' => $ex->getMessage()
-            ];
-        }
-    }
-
-    /**
-     * 
-     * @param int|string $id
-     * @param stdClass|NULL $extra
-     * @return array
-     */
-    public function Delete($id, $extra = null)
-    {
-        $Id = (int) $id;
-        try {
-            return [
-                'success' => true,
-                'id' => $this
-                        ->getEntityManager()
-                        ->getRepository('Core\Entity\Vocation')
-                        ->Delete($Id, $extra)
-            ];
-        } catch (Exception $ex) {
-            return [
-                'success' => false,
-                'message' => $ex->getMessage()
-            ];
-        }
-    }
+    protected $baseEntity = 'Core\Entity\Vocation';
 
 }
