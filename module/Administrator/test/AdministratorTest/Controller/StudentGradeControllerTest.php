03333bf85953d3d281532a025c5589228f8c096e
Eleri Apsolon <eleri.apsolon@gmail.com>
2016-01-25 10:49:26 +0200

    #37 Finished testing

--- module/Administrator/test/AdministratorTest/Controller/StudentGradeControllerTest.php
+++ module/Administrator/test/AdministratorTest/Controller/StudentGradeControllerTest.php
@@ -44,7 +44,7 @@ class StudentGradeControllerTest extends UnitHelpers
     /**
      * imitate POST request
      * create new with correct data see entity
-     * creates new row to databaase
+     * creates new row to database
      */
     public function testCreateWithContactLesson()
     {
@@ -67,7 +67,7 @@ class StudentGradeControllerTest extends UnitHelpers
     /**
      * imitate POST request
      * create new with correct data see entity
-     * creates new row to databaase
+     * creates new row to database
      */
     public function testCreateWithModule()
     {
@@ -90,7 +90,7 @@ class StudentGradeControllerTest extends UnitHelpers
     /**
      * imitate POST request
      * create new with correct data see entity
-     * creates new row to databaase
+     * creates new row to database
      */
     public function testCreateWithSubjectRound()
     {
@@ -113,7 +113,7 @@ class StudentGradeControllerTest extends UnitHelpers
     /**
      * imitate POST request
      * create new with correct data see entity
-     * creates new row to databaase
+     * creates new row to database
      */
     public function testCreateWithIndependentWork()
     {
@@ -133,6 +133,9 @@ class StudentGradeControllerTest extends UnitHelpers
         $this->assertEquals(1, $result->success);
     }
 
+    /**
+     * Giving grade to nothing
+     */
     public function testCreateWithNoSpecification()
     {
         $notes = 'StudentGrade notes' . uniqid();
@@ -169,6 +172,9 @@ class StudentGradeControllerTest extends UnitHelpers
         $this->assertNotEquals(1, $result->success);
     }
 
+    /**
+     * create one before getting
+     */
     public function testGet()
     {
         $this->request->setMethod('get');
@@ -180,6 +186,9 @@ class StudentGradeControllerTest extends UnitHelpers
         $this->PrintOut($result, false);
     }
 
+    /**
+     * create one before asking list
+     */
     public function testGetList()
     {
         $this->CreateStudentGrade();
@@ -234,6 +243,9 @@ class StudentGradeControllerTest extends UnitHelpers
         $this->assertNotEquals($teacherIdOld, $result->data['teacher']['id']);
     }
 
+    /**
+     * Test with wrong data
+     */
     public function testUpdateFalseData()
     {
         $data = [
@@ -267,18 +279,180 @@ class StudentGradeControllerTest extends UnitHelpers
             'gradeChoice' => $this->CreateGradeChoice()->getId(),
             'teacher' => $this->CreateTeacher()->getId(),
             'module' => $this->CreateModule()->getId(),
-        ]));
-
-        
+        ]));        
         
         //fire request
         $result = $this->controller->dispatch($this->request);
         $response = $this->controller->getResponse();
         
-        $this->PrintOut($result, true);
+        $this->PrintOut($result, false);
         
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertNotEquals(1, $result->success);
+    }
+    
+    /**
+     * TEST row gets deleted by id
+     * can only try to delete smt what is trashed
+     */
+    public function testDeleteNotTrashed()
+    {
+        $entity = $this->CreateStudentGrade();
+        $idOld = $entity->getId();
+
+        $this->routeMatch->setParam('id', $entity->getId());
+        $this->request->setMethod('delete');
+
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+        $this->PrintOut($result, false);
+
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertNotEquals(1, $result->success);
+        $this->em->clear();
+
+        //test it is not in the database anymore
+        $deleted = $this->em
+                ->getRepository('Core\Entity\StudentGrade')
+                ->Get($idOld);
+
+        $this->assertNotEquals(null, $deleted);
+    }
+    
+    public function testDelete()
+    {
+
+        $entity = $this->CreateStudentGrade();
+        $idOld = $entity->getId();
+        $entity->setTrashed(1);
+        $this->em->persist($entity);
+        $this->em->flush($entity);
+
+        $this->routeMatch->setParam('id', $entity->getId());
+        $this->request->setMethod('delete');
+
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(1, $result->success);
+        $this->em->clear();
+
+        //test if it is not in the database anymore
+        $deleted = $this->em
+                ->getRepository('Core\Entity\StudentGrade')
+                ->Get($idOld);
+
+        $this->assertEquals(null, $deleted);
+
+        $this->PrintOut($result, false);
+    }
+    
+    public function testCreatedByAndUpdatedBy()
+    {
+        $this->request->setMethod('post');
+
+        $notes = 'StudentGrade notes' . uniqid();
+        $studentGrade = $this->CreateStudentGrade()->getId();
+        
+        $student = $this->CreateStudent()->getId();
+        $contactLesson = $this->CreateContactLesson()->getId();
+        $teacher = $this->CreateTeacher()->getId();
+        $gradeChoice = $this->CreateGradeChoice()->getId();
+
+        $this->request->getPost()->set('notes', $notes);
+        $this->request->getPost()->set('student', $student);
+        $this->request->getPost()->set('studentGrade', $studentGrade);
+        $this->request->getPost()->set('contactLesson', $contactLesson);
+        $this->request->getPost()->set('teacher', $teacher);
+        $this->request->getPost()->set('gradeChoice', $gradeChoice);
+
+        $lisUserCreates = $this->CreateLisUser();
+        $lisUserCreatesId = $lisUserCreates->getId();
+        $this->request->getPost()->set("createdBy", $lisUserCreatesId);
+
+        $lisUserUpdates = $this->CreateLisUser();
+        $lisUserUpdatesId = $lisUserUpdates->getId();
+        $this->request->getPost()->set("updatedBy", $lisUserUpdatesId);
+
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(1, $result->success);
+
+        $this->PrintOut($result, false);
+
+        $repository = $this->em->getRepository('Core\Entity\StudentGrade');
+        $newSubjectRound = $repository->find($result->data['id']);
+        $this->assertEquals($lisUserCreatesId, $newSubjectRound->getCreatedBy()->getId());
+        $this->assertEquals($lisUserUpdatesId, $newSubjectRound->getUpdatedBy()->getId());
+    }
+    
+    /**
+     * TEST rows get read by limit and page params
+     */
+    public function testGetListWithPaginaton()
+    {
+        $this->request->setMethod('get');
+
+        //set record limit to 1
+        $q = 'page=1&limit=1'; //imitate real param format
+        $params = [];
+        parse_str($q, $params);
+        foreach ($params as $key => $value) {
+            $this->request->getQuery()->set($key, $value);
+        }
+
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(1, $result->success);
+        $this->assertLessThanOrEqual(1, count($result->data));
+        $this->PrintOut($result, false);
+    }
+    
+    public function testGetTrashedList()
+    {
+
+        //prepare one AbsenceReason with trashed flag set up
+        $entity = $this->CreateStudentGrade();
+        $entity->setTrashed(1);
+        $this->em->persist($entity);
+        $this->em->flush($entity); //save to db with trashed 1
+        $where = [
+            'trashed' => 1,
+            'id' => $entity->getId()
+        ];
+        $whereJSON = Json::encode($where);
+        $whereURL = urlencode($whereJSON);
+        $whereURLPart = "where=$whereURL";
+        $q = "page=1&limit=1&$whereURLPart"; //imitate real param format
+
+        $params = [];
+        parse_str($q, $params);
+        foreach ($params as $key => $value) {
+            $this->request->getQuery()->set($key, $value);
+        }
+
+        $this->request->setMethod('get');
+
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->PrintOut($result, true);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(1, $result->success);
+
+        //limit is set to 1
+        $this->assertEquals(1, count($result->data));
+
+        //assert all results have trashed not null
+        foreach ($result->data as $value) {
+            $this->assertEquals(1, $value['trashed']);
+        }
     }
 
 }
