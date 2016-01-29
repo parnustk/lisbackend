a2df9c78bd9dc22f86f5e9b8bd349049342855e3
Arnold <tserepov@gmail.com>
2016-01-29 20:44:05 +0200

    #20 res confl

--- public/app/administrator/lis/src/app/config.js
+++ public/app/administrator/lis/src/app/config.js
@@ -1,23 +1,12 @@
-
-/* global define */
-
-/* 
- * LIS development
- * @link      https://github.com/parnustk/lisbackend
- * @copyright Copyright (c) 2015-2016 Sander Mets, Eleri Apsolon, Arnold Tšerepov, Marten Kähr, Kristen Sepp, Alar Aasa, Juhan Kõks
- * @license   https://github.com/parnustk/lisbackend/blob/master/LICENSE.txt
- * @author 
- */
-
 (function (define) {
     'use strict';
-
+    
     /**
      * 
      * @returns {config_L12.config_L18.config}
      */
     define([], function () {
-
+        
         /**
          * 
          * @param {Object} $routeProvider
@@ -27,25 +16,31 @@
         function config($routeProvider, $locationProvider) {
 
             $routeProvider
+            
                     .when('/vocation', {
                         templateUrl: 'lis/dist/templates/vocation.html',
                         controller: 'vocationController'
                     })
+                    
                     .when('/teacher', {
                         templateUrl: 'lis/dist/templates/teacher.html',
                         controller: 'teacherController'
                     })
+                    
                     .when('/gradingtype', {
                         templateUrl: 'lis/dist/templates/gradingType.html',
                         controller: 'gradingTypeController'
                     })
+                    
                     .when('/absencereason', {
                         templateUrl: 'lis/dist/templates/absencereason.html',
                         controller: 'absencereasonController'})
+                    
                     .when('/gradechoice', {
                         templateUrl: 'lis/dist/templates/gradechoice.html', 
                         controller: 'gradechoiceController'})
-                    .otherwise({redirectTo: '/'});
+                    
+                .otherwise({redirectTo: '/'});
 
             $locationProvider.html5Mode({
                 enabled: false,
