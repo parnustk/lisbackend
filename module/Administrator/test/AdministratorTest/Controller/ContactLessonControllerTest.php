3204ffb5efa50c550d447fd151899743a8f933d5
Eleri Apsolon <eleri.apsolon@gmail.com>
2016-01-29 12:06:00 +0200

    #25 todo fetch

--- module/Administrator/test/AdministratorTest/Controller/ContactLessonControllerTest.php
+++ module/Administrator/test/AdministratorTest/Controller/ContactLessonControllerTest.php
@@ -30,45 +30,45 @@ class ContactLessonTest extends UnitHelpers
         parent::setUp();
     }
 
-    public function testCreateNoData()
-    {
-        $this->request->setMethod('post');
-        $result = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-        $this->PrintOut($result, false);
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(false, (bool) $result->success);
-    }
-
-    public function testCreate()
-    {
-        $this->request->setMethod('post');
-        $lessonDate = new \DateTime;
-        $this->request->getPost()->set("lessonDate", $lessonDate);
-        $description = uniqid() . ' Description for contactlesson';
-        $this->request->getPost()->set("description", $description);
-        $durationAK = 4;
-        $this->request->getPost()->set("durationAK", $durationAK);
-
-        $subjectRound = $this->CreateSubjectRound();
-
-        $this->request->getPost()->set("subjectRound", $subjectRound->getId());
-
-        $teachers = [];
-        foreach ($subjectRound->getTeacher() as $teacher) {
-            $teachers[] = [
-                'id' => $teacher->getId()
-            ];
-        }
-        $this->request->getPost()->set("teacher", $teachers);
-        $result = $this->controller->dispatch($this->request);
-        $this->PrintOut($result, false);
-        $response = $this->controller->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(1, $result->success);
-        $this->PrintOut($result, false);
-    }
+//    public function testCreateNoData()
+//    {
+//        $this->request->setMethod('post');
+//        $result = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//        $this->PrintOut($result, false);
+//
+//        $this->assertEquals(200, $response->getStatusCode());
+//        $this->assertEquals(false, (bool) $result->success);
+//    }
+//
+//    public function testCreate()
+//    {
+//        $this->request->setMethod('post');
+//        $lessonDate = new \DateTime;
+//        $this->request->getPost()->set("lessonDate", $lessonDate);
+//        $description = uniqid() . ' Description for contactlesson';
+//        $this->request->getPost()->set("description", $description);
+//        $durationAK = 4;
+//        $this->request->getPost()->set("durationAK", $durationAK);
+//
+//        $subjectRound = $this->CreateSubjectRound();
+//
+//        $this->request->getPost()->set("subjectRound", $subjectRound->getId());
+//
+//        $teachers = [];
+//        foreach ($subjectRound->getTeacher() as $teacher) {
+//            $teachers[] = [
+//                'id' => $teacher->getId()
+//            ];
+//        }
+//        $this->request->getPost()->set("teacher", $teachers);
+//        $result = $this->controller->dispatch($this->request);
+//        $this->PrintOut($result, false);
+//        $response = $this->controller->getResponse();
+//        $this->assertEquals(200, $response->getStatusCode());
+//        $this->assertEquals(1, $result->success);
+//        $this->PrintOut($result, false);
+//    }
 
     public function testUpdate()
     {
@@ -117,10 +117,11 @@ class ContactLessonTest extends UnitHelpers
         //fire request
         $result = $this->controller->dispatch($this->request);
         $response = $this->controller->getResponse();
+        $this->PrintOut($result, true);
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals(1, $result->success);
 
-        $this->PrintOut($result, false);
+        
 
         //start checking for changed data
         $this->assertNotEquals($lessonDateO, $result->data['lessonDate']);
@@ -136,141 +137,205 @@ class ContactLessonTest extends UnitHelpers
         }
     }
 
-    public function testGet()
-    {
-        $this->request->setMethod('get');
-        $this->routeMatch->setParam('id', $this->CreateContactLesson()->getId());
-        $result = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(1, $result->success);
-        $this->PrintOut($result, false);
-    }
+//    public function testGet()
+//    {
+//        $this->request->setMethod('get');
+//        $this->routeMatch->setParam('id', $this->CreateContactLesson()->getId());
+//        $result = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//        $this->assertEquals(200, $response->getStatusCode());
+//        $this->assertEquals(1, $result->success);
+//        $this->PrintOut($result, false);
+//    }
 
-    public function testGetList()
-    {
-
-        $this->CreateContactLesson();
-        $this->request->setMethod('get');
-        $result = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(1, $result->success);
-        $this->assertGreaterThan(0, count($result->data));
-        $this->PrintOut($result, false);
-    }
-
-    public function testDeleteNotTrashed()
-    {
-        $entity = $this->CreateContactLesson();
-        $idOld = $entity->getId();
-
-        $this->routeMatch->setParam('id', $entity->getId());
-        $this->request->setMethod('delete');
-
-        $result = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-        $this->PrintOut($result, false);
-
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertNotEquals(1, $result->success);
-        $this->em->clear();
-
-        //test it is not in the database anymore
-        $deleted = $this->em
-                ->getRepository('Core\Entity\ContactLesson')
-                ->Get($idOld);
-
-        $this->assertNotEquals(null, $deleted);
-    }
-
-    public function testDelete()
-    {
-        $entity = $this->CreateContactLesson();
-        $idOld = $entity->getId();
-        $entity->setTrashed(1);
-        $this->em->persist($entity);
-        $this->em->flush($entity);
-
-        $this->routeMatch->setParam('id', $entity->getId());
-        $this->request->setMethod('delete');
-
-        $result = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(1, $result->success);
-        $this->em->clear();
-
-        //test if it is not in the database anymore
-        $deleted = $this->em
-                ->getRepository('Core\Entity\ContactLesson')
-                ->Get($idOld);
-
-        $this->assertEquals(null, $deleted);
-
-        $this->PrintOut($result, false);
-    }
-
-    public function testCreatedByAndUpdatedBy()
-    {
-//        $this->request->setMethod('post');
+//    public function testGetList()
+//    {
 //
-//        $description = 'ContactLesson description' . uniqid();
-//        $lessonDate = 'ContactLesson lessonDate' . uniqid();
-//        $durationAK = 'ContactLesson durationAK' . uniqid();
+//        $this->CreateContactLesson();
+//        $this->request->setMethod('get');
+//        $result = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//        $this->assertEquals(200, $response->getStatusCode());
+//        $this->assertEquals(1, $result->success);
+//        $this->assertGreaterThan(0, count($result->data));
+//        $this->PrintOut($result, false);
+//    }
+
+//    public function testDeleteNotTrashed()
+//    {
+//        $entity = $this->CreateContactLesson();
+//        $idOld = $entity->getId();
 //
-//        $contactLesson = $this->CreateContactLesson()->getId();
+//        $this->routeMatch->setParam('id', $entity->getId());
+//        $this->request->setMethod('delete');
 //
-//        $absence = $this->CreateAbsence()->getId();
-//        $subjectRound = $this->CreateSubjectRound()->getId();
-//        
-//        $teacher = $this->CreateTeacher()->getId();     
-//        $rooms = $this->CreateRoom()->getId();
-//        
-//        $studentGrade = $this->CreateStudentGrade()->getId();
+//        $result = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//        $this->PrintOut($result, false);
 //
-//        $this->request->getPost()->set('description', $description);
-//        $this->request->getPost()->set('lessonDate', $lessonDate);
-//        $this->request->getPost()->set('durationAK', $durationAK);
-//        $this->request->getPost()->set('contactLesson', $contactLesson);
-//        $this->request->getPost()->set('absence', $absence);
-//        $this->request->getPost()->set('subjectRound', $subjectRound);
-//        $teacher = [];
-//        foreach ($subjectRound->getTeacher() as $teachers) {
-//            $teacher[] = [
-//                'id' => $teachers->getId()
-//            ];
-//        }
-//        $rooms = [];
-//        foreach ($subjectRound->getRooms() as $room) {
-//            $rooms[] = [
-//                'id' => $room->getId()
-//            ];
-//        }
-//        $this->request->getPost()->set('studentGrade', $studentGrade);
 //
-//        $lisUserCreates = $this->CreateLisUser();
-//        $lisUserCreatesId = $lisUserCreates->getId();
-//        $this->request->getPost()->set("createdBy", $lisUserCreatesId);
+//        $this->assertEquals(200, $response->getStatusCode());
+//        $this->assertNotEquals(1, $result->success);
+//        $this->em->clear();
 //
-//        $lisUserUpdates = $this->CreateLisUser();
-//        $lisUserUpdatesId = $lisUserUpdates->getId();
-//        $this->request->getPost()->set("updatedBy", $lisUserUpdatesId);
+//        //test it is not in the database anymore
+//        $deleted = $this->em
+//                ->getRepository('Core\Entity\ContactLesson')
+//                ->Get($idOld);
+//
+//        $this->assertNotEquals(null, $deleted);
+//    }
+
+//    public function testDelete()
+//    {
+//        $entity = $this->CreateContactLesson();
+//        $idOld = $entity->getId();
+//        $entity->setTrashed(1);
+//        $this->em->persist($entity);
+//        $this->em->flush($entity);
+//
+//        $this->routeMatch->setParam('id', $entity->getId());
+//        $this->request->setMethod('delete');
 //
 //        $result = $this->controller->dispatch($this->request);
 //        $response = $this->controller->getResponse();
 //
 //        $this->assertEquals(200, $response->getStatusCode());
 //        $this->assertEquals(1, $result->success);
+//        $this->em->clear();
+//
+//        //test if it is not in the database anymore
+//        $deleted = $this->em
+//                ->getRepository('Core\Entity\ContactLesson')
+//                ->Get($idOld);
+//
+//        $this->assertEquals(null, $deleted);
 //
 //        $this->PrintOut($result, false);
+//    }
+
+//    public function testCreatedByAndUpdatedBy()
+//    {
+////        $this->request->setMethod('post');
+////
+////        $description = 'ContactLesson description' . uniqid();
+////        $lessonDate = new \DateTime;
+////        $durationAK = 5;
+////
+////        $contactLesson = $this->CreateContactLesson()->getId();
+////
+////        $absence = $this->CreateAbsence()->getId();
+////        $subjectRound = $this->CreateSubjectRound()->getId();
+////        $studentGrade = $this->CreateStudentGrade()->getId();
+////        
+//////        $teacher = $this->CreateTeacher()->getId();     
+//////        $rooms = $this->CreateRoom()->getId();
+////
+////        $this->request->getPost()->set('description', $description);
+////        $this->request->getPost()->set('lessonDate', $lessonDate);
+////        $this->request->getPost()->set('durationAK', $durationAK);
+////        $this->request->getPost()->set('contactLesson', $contactLesson);
+////        $this->request->getPost()->set('absence', $absence);
+////        $this->request->getPost()->set('subjectRound', $subjectRound);
+//////        $teacher = [];
+//////        foreach ($subjectRound->getTeacher() as $teachers) {
+//////            $teacher[] = [
+//////                'id' => $teachers->getId()
+//////            ];
+//////        }
+//////        $rooms = [];
+//////        foreach ($subjectRound->getRooms() as $room) {
+//////            $rooms[] = [
+//////                'id' => $room->getId()
+//////            ];
+//////        }
+////        $this->request->getPost()->set('studentGrade', $studentGrade);
+////
+////        $lisUserCreates = $this->CreateLisUser();
+////        $lisUserCreatesId = $lisUserCreates->getId();
+////        $this->request->getPost()->set("createdBy", $lisUserCreatesId);
+////
+////        $lisUserUpdates = $this->CreateLisUser();
+////        $lisUserUpdatesId = $lisUserUpdates->getId();
+////        $this->request->getPost()->set("updatedBy", $lisUserUpdatesId);
+////
+////        $result = $this->controller->dispatch($this->request);
+////        $response = $this->controller->getResponse();
+////
+////        $this->assertEquals(200, $response->getStatusCode());
+////        $this->assertEquals(1, $result->success);
+////
+////        $this->PrintOut($result, false);
+////
+////        $repository = $this->em->getRepository('Core\Entity\ContactLesson');
+////        $newContactLesson = $repository->find($result->data['id']);
+////        $this->assertEquals($lisUserCreatesId, $newContactLesson->getCreatedBy()->getId());
+////        $this->assertEquals($lisUserUpdatesId, $newContactLesson->getUpdatedBy()->getId());
+//    }
+    
+    /**
+     * TEST rows get read by limit and page params
+     */
+//    public function testGetListWithPaginaton()
+//    {
+//        $this->request->setMethod('get');
 //
-//        $repository = $this->em->getRepository('Core\Entity\ContactLesson');
-//        $newContactLesson = $repository->find($result->data['id']);
-//        $this->assertEquals($lisUserCreatesId, $newContactLesson->getCreatedBy()->getId());
-//        $this->assertEquals($lisUserUpdatesId, $newContactLesson->getUpdatedBy()->getId());
+//        //set record limit to 1
+//        $q = 'page=1&limit=1'; //imitate real param format
+//        $params = [];
+//        parse_str($q, $params);
+//        foreach ($params as $key => $value) {
+//            $this->request->getQuery()->set($key, $value);
+//        }
+//
+//        $result = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//        $this->assertEquals(200, $response->getStatusCode());
+//        $this->assertEquals(1, $result->success);
+//        $this->assertLessThanOrEqual(1, count($result->data));
+//        $this->PrintOut($result, false);
+//    }
+    
+    public function testGetTrashedList()
+    {
+
+        //prepare one AbsenceReason with trashed flag set up
+//        $entity = $this->CreateContactLesson();
+//        $entity->setTrashed(1);
+//        $this->em->persist($entity);
+//        $this->em->flush($entity); //save to db with trashed 1
+//        $where = [
+//            'trashed' => 1,
+//            'id' => $entity->getId()
+//        ];
+//        $whereJSON = Json::encode($where);
+//        $whereURL = urlencode($whereJSON);
+//        $whereURLPart = "where=$whereURL";
+//        $q = "page=1&limit=1&$whereURLPart"; //imitate real param format
+//
+//        $params = [];
+//        parse_str($q, $params);
+//        foreach ($params as $key => $value) {
+//            $this->request->getQuery()->set($key, $value);
+//        }
+//
+//        $this->request->setMethod('get');
+//
+//        $result = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//
+//        $this->PrintOut($result, true);
+//
+//        $this->assertEquals(200, $response->getStatusCode());
+//        $this->assertEquals(1, $result->success);
+//
+//        //limit is set to 1
+//        $this->assertEquals(1, count($result->data));
+//
+//        //assert all results have trashed not null
+//        foreach ($result->data as $value) {
+//            $this->assertEquals(1, $value['trashed']);
+//        }
     }
 
 }
