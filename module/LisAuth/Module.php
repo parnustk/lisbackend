04c68344bfde8686a47f67df2cae3658966ba90c
Sander Mets <sandermets@live.com>
2016-02-07 21:11:51 +0200

    #49 done sceleton for LisAuth

---
+++ module/LisAuth/Module.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Licence of Learning Info System (LIS)
+ * 
+ * @link      https://github.com/parnustk/lisbackend
+ * @copyright Copyright (c) 2015-2016 Sander Mets, Eleri Apsolon, Arnold Tšerepov, Marten Kähr, Kristen Sepp, Alar Aasa, Juhan Kõks
+ * @license   https://github.com/parnustk/lisbackend/blob/master/LICENSE
+ */
+
+namespace LisAuth;
+
+/**
+ * @author Sander Mets <sandermets0@gmail.com>
+ */
+class Module
+{
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+}
---
+++ module/LisAuth/src/LisAuth/Controller/RegisterAdministratorController.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Licence of Learning Info System (LIS)
+ * 
+ * @link      https://github.com/parnustk/lisbackend
+ * @copyright Copyright (c) 2015-2016 Sander Mets, Eleri Apsolon, Arnold Tšerepov, Marten Kähr, Kristen Sepp, Alar Aasa, Juhan Kõks
+ * @license   https://github.com/parnustk/lisbackend/blob/master/LICENSE
+ */
+
+namespace LisAuth\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+/**
+ * @author Sander Mets <sandermets0@gmail.com>
+ */
+class RegisterAdministratorController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+}
