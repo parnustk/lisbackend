cb7a2ece5cc036d2c855231282f965e93a7c157f
arnoldtserepov <tserepov@gmail.com>
2016-02-07 13:53:34 +0200

    #20 Added comments in Controller

--- module/Administrator/src/Administrator/Controller/GradeChoiceController.php
+++ module/Administrator/src/Administrator/Controller/GradeChoiceController.php
@@ -4,8 +4,8 @@
  * LIS development
  *
  * @link      https://github.com/parnustk/lisbackend
- * @copyright Copyright (c) 2016 LIS dev team
- * @license   https://opensource.org/licenses/MIT MIT License
+ * @copyright Copyright (c) 2015-2016 Sander Mets, Eleri Apsolon, Arnold Tšerepov, Marten Kähr, Kristen Sepp, Alar Aasa, Juhan Kõks
+ * @license   https://github.com/parnustk/lisbackend/blob/master/LICENSE.txt
  */
 
 namespace Administrator\Controller;
@@ -20,7 +20,7 @@ use Core\Controller\AbstractBaseController;
  */
 class GradeChoiceController extends AbstractBaseController {
 
-     /**
+    /**
      *
      * @var type 
      */
@@ -28,58 +28,68 @@ class GradeChoiceController extends AbstractBaseController {
 
     /**
      * GET
-     *
+     * <h2>GET admin/absencereason</h2>
+     * <h3>URL Parameters</h3>
+     * <code>limit(integer)
+     * page(integer)</code>
+     * 
      * @return JsonModel
      */
-    public function getList()
-    {
+    public function getList() {
         return parent::getList();
     }
 
     /**
-     * GET
-     * 
+     * <h2>GET admin/absencereason/:id</h2>
+     * <h3>URL Parameters</h3>
+     * <code>id(integer)*</code>
      * @param type $id
      * @return JsonModel
      */
-    public function get($id)
-    {
+    public function get($id) {
         return parent::get($id);
     }
 
     /**
      * POST
+     * <h2>POST admin/absencereason</h2>
+     * <h3>Body</h3>
+     * <code>name(string)*</code
      * 
      * @param type $data
      * @return JsonModel
      */
-    public function create($data)
-    {
+    public function create($data) {
         return parent::create($data);
     }
 
     /**
      * PUT
+     * <h2>PUT admin/absencereason/:id</h2>
+     * <h3>URL Parameters</h3>
+     * <code>id(integer)*</code>
+     * <h3>Body</h3>
+     * <code>name(string)*</code>
      * 
      * @param type $id
      * @param type $data
      * @return JsonModel
      */
-    public function update($id, $data)
-    {
+    public function update($id, $data) {
         return parent::update($id, $data);
     }
 
     /**
      * DELETE
+     *  <h2>DELETE admin/absencereason/:id</h2>
+     * <h3>URL Parameters</h3>
+     * <code>id(integer)*</code>
      * 
      * @param int $id
      * @return JsonModel
      */
-    public function delete($id)
-    {
+    public function delete($id) {
         return parent::delete($id);
     }
 
 }
-
