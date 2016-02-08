0b7188595c3fa98fd74522124584fe2fb71cddb1
Eleri Apsolon <eleri.apsolon@gmail.com>
2016-02-08 10:59:18 +0200

    #47 Modified Controller tests

--- module/Administrator/test/AdministratorTest/Controller/ModuleControllerTest.php
+++ module/Administrator/test/AdministratorTest/Controller/ModuleControllerTest.php
@@ -331,7 +331,7 @@ class ModuleControllerTest extends UnitHelpers
         $result = $this->controller->dispatch($this->request);
         $response = $this->controller->getResponse();
 
-        $this->PrintOut($result, true);
+        $this->PrintOut($result, false);
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals(1, $result->success);
