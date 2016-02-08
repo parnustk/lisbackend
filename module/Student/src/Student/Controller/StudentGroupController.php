70cd5276bb140d2750bf5a4c97587a7f70179f3d
Eleri Apsolon <eleri.apsolon@gmail.com>
2016-02-08 13:50:31 +0200

    #56 Implementing StudentGroup for Student and Teacher Module

---
+++ module/Student/src/Student/Controller/StudentGroupController.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Licence of Learning Info System (LIS)
+ * 
+ * @link      https://github.com/parnustk/lisbackend
+ * @copyright Copyright (c) 2015-2016 Sander Mets, Eleri Apsolon, Arnold Tšerepov, Marten Kähr, Kristen Sepp, Alar Aasa, Juhan Kõks
+ * @license   https://github.com/parnustk/lisbackend/blob/master/LICENSE.txt
+ */
+
+namespace Student\Controller;
+
+use Zend\View\Model\JsonModel;
+use Core\Controller\AbstractBaseController;
+
+/**
+ * @author Eleri Apsolon <eleri.apsolon@gmail.com>
+ */
+class StudentGroupController extends AbstractBaseController
+{
+    
+    /**
+     *
+     * @var type 
+     */
+    protected $service = 'studentgroup_service';
+
+    /**
+     * <h2>GET student/studentgroup</h2>
+     * <h3>URL Parameters</h3>
+     * <code>limit(integer)
+     * page(integer)</code>
+     * 
+     * @return JsonModel
+     */
+    public function getList()
+    {
+        return parent::getList();
+    }
+
+    /**
+     * <h2>GET student/studentgroup/:id</h2>
+     * <h3>URL Parameters</h3>
+     * <code>id(integer)*</code>
+     * 
+     * @param type $id
+     * @return JsonModel
+     */
+    public function get($id)
+    {
+        return parent::get($id);
+    }
+
+    /**
+     * <h2>POST student/studentgroup</h2>
+     * <h3>Body</h3>
+     * <code>name(string)*
+     * vocation(integer)</code>
+     * 
+     * @param array $data
+     * @return JsonModel
+     */
+    public function create($data)
+    {
+        return parent::notAllowed();
+    }
+
+    /**
+     * <h2>PUT student/studentgroup/:id</h2>
+     * <h3>URL Parameters</h3>
+     * <code>id(integer)*</code>
+     * <h3>Body</h3>
+     * <code>name(string)*
+     * vocation(integer)*</code>
+     * 
+     * @param type $id
+     * @param type $data
+     * @return JsonModel
+     */
+    public function update($id, $data)
+    {
+        return parent::notAllowed();
+    }
+
+    /**
+     * <h2>DELETE student/studentgroup/:id</h2>
+     * <h3>URL Parameters</h3>
+     * <code>id(integer)*</code>
+     * 
+     * @param int $id
+     * @return JsonModel
+     */
+    public function delete($id)
+    {
+        return parent::notAllowed();
+    }
+
+}
