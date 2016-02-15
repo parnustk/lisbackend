f620065bb6154af33480feaaf7ecc89a1fa4cab7
Eleri Apsolon <eleri.apsolon@gmail.com>
2016-02-15 20:55:22 +0200

    #66 Starting ContactLesson in student module

---
+++ module/Teacher/src/Teacher/Controller/ContactLessonController.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Licence of Learning Info System (LIS)
+ * 
+ * @link      https://github.com/parnustk/lisbackend
+ * @copyright Copyright (c) 2015-2016 Sander Mets, Eleri Apsolon, Arnold Tšerepov, Marten Kähr, Kristen Sepp, Alar Aasa, Juhan Kõks
+ * @license   https://github.com/parnustk/lisbackend/blob/master/LICENSE.txt
+ */
+
+namespace Teacher\Controller;
+
+use Zend\View\Model\JsonModel;
+use Core\Controller\AbstractBaseController;
+
+/**
+ * @author Eleri Apsolon <eleri.apsolon@gmail.com>
+ */
+class ContactLessonController extends AbstractBaseController
+{
+    
+    /**
+     *
+     * @var type 
+     */
+    protected $service = 'contactlesson_service';
+
+    /**
+     * <h2>GET teacher/contactlesson</h2>
+     * <h3>URL Parameters</h3>
+     * <code>limit(integer)
+     * page(integer)</code>
+     * 
+     * @return JsonModel
+     */
+    public function getList()
+    {
+        return parent::getList();
+    }
+
+    /**
+     * <h2>GET teacher/contactlesson/:id</h2>
+     * <h3>URL Parameters</h3>
+     * <code>id(integer)*</code>
+     * 
+     * @param int $id
+     * @return JsonModel
+     */
+    public function get($id)
+    {
+        return parent::get($id);
+    }
+
+    /**
+     * <h2>POST teacher/contactlesson</h2>
+     * <h3>Body</h3>
+     * <code> lessonDate(datetime)*
+     * description(string)*
+     * durationAK(integer)*
+     * subjectRound(array) [ { id(integer) } ] ]*
+     * teacher(array) [ { id(integer) } ] ]*</code>
+     * 
+     * @param array $data
+     * @return JsonModel
+     */
+    public function create($data)
+    {
+        return parent::notAllowed();
+    }
+
+    /**
+     * <h2>PUT teacher/contactlesson/:id</h2>
+     * <h3>URL Parameters</h3>
+     * <code>id(integer)*</code>
+     * <h3>Body</h3>
+     * <code> lessonDate(datetime)*
+     * description(string)*
+     * durationAK(integer)*
+     * subjectRound(array) [ { id(integer) } ] ]*
+     * teacher(array) [ { id(integer) } ] ]*</code>
+     * @param int $id
+     * @return JsonModel
+     */
+    public function update($id, $data)
+    {
+        return parent::notAllowed();
+    }
+
+    /**
+     * <h2>DELETE teacher/contactlesson/:id</h2>
+     * <h3>URL Parameters</h3>
+     * <code>id(integer)*</code>
+     * 
+     * @param int $id
+     * @return JsonModel
+     */
+    public function delete($id)
+    {
+        return parent::notAllowed();
+    }
+
+}
