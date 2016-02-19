279c942d07b87332a3e7175fd482645a3f00d925
Juhan Kõks <juhankoks@gmail.com>
2016-02-19 16:30:16 +0200

    #79 fixing Licence info

--- module/Administrator/test/AdministratorTest/Controller/TeacherControllerTest.php
+++ module/Administrator/test/AdministratorTest/Controller/TeacherControllerTest.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * LIS development
+ *
+ * @link      https://github.com/parnustk/lisbackend
+ * @copyright Copyright (c) 2015-2016 Sander Mets, Eleri Apsolon, Arnold Tšerepov, Marten Kähr, Kristen Sepp, Alar Aasa, Juhan Kõks
+ * @license   https://github.com/parnustk/lisbackend/blob/master/LICENSE.txt
+ */
+
 namespace AdministratorTest\Controller;
 
 use Administrator\Controller\TeacherController;
