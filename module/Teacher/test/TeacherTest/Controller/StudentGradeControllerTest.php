998c6745ac11509b5838eaa68c5fad07caa4d6f6
marten <marten@kahr.ee>
2016-02-29 09:10:54 +0200

    #89 Create Test finished; works.

--- module/Teacher/test/TeacherTest/Controller/StudentGradeControllerTest.php
+++ module/Teacher/test/TeacherTest/Controller/StudentGradeControllerTest.php
@@ -10,6 +10,10 @@
 
 namespace TeacherTest\Controller;
 
+use Teacher\Controller\StudentGradeController;
+use Zend\Json\Json;
+use TeacherTest\UnitHelpers;
+
 /**
  * Description of StudentGradeControllerTest
  *
@@ -25,4 +29,42 @@ class StudentGradeControllerTest extends UnitHelpers
         $this->controller = new StudentGradeController();
         parent::setUp();
     }
+    
+//    /**
+//     * should be successful
+//     */
+//    public function testCreate()
+//    {
+//        //create and set correct teacheruser
+//        $teacher = $this->CreateTeacher();
+//        $lisUser = $this->CreateTeacherUser($teacher);
+//
+//        //set to current controller
+//        $this->controller->setLisUser($lisUser);
+//        $this->controller->setLisPerson($teacher);
+//
+//        //add data
+//        $notes = 'Notes' . uniqid();
+//        $this->request->setMethod('post');
+//        $this->request->getPost()->set('notes', $notes);
+//        $this->request->getPost()->set('student', $this->CreateStudent()->getId());
+//        $this->request->getPost()->set('gradechoice', $this->CreateContactLesson()->getId());
+//        $this->request->getPost()->set('teacher', $teacher->getId());
+//        $this->request->getPost()->set('independentwork', $this->CreateIndependentWork()->getId());
+//        $this->request->getPost()->set('module', $this->CreateModule()->getId());
+//        $this->request->getPost()->set('subjectround', $this->CreateSubjectRound()->getId());
+//        $this->request->getPost()->set('contactlesson', $this->CreateContactLesson()->getId());
+//
+//        //fire request
+//        $result = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//
+//        $this->PrintOut($result, false);
+//
+//        //make assertions
+//        $this->assertEquals(200, $response->getStatusCode());
+//        $this->assertEquals(true, (bool) $result->success);
+//    }
+    
+    
 }
