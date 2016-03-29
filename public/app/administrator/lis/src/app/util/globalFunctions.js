6e0cccebf5b15fcdc4fdc626adffd6c7e8eaee9f
Eleri Apsolon <eleri.apsolon@gmail.com>
2016-03-29 13:16:53 +0300

    #184

---
+++ public/app/administrator/lis/src/app/util/globalFunctions.js
@@ -0,0 +1,55 @@
+/**
+ * LIS development
+ *
+ * @link      https://github.com/parnustk/lisbackend
+ * @copyright Copyright (c) 2016 LIS dev team
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+/**
+ * Holds common functions used application wide
+ * @returns {undefined}
+ */
+(function () {
+    'use strict';
+
+    define([], function () {
+        
+        return {
+            /**
+             * Leaves only id property for sub level objects
+             * required by Doctrine to work
+             * Needed for models which have associotions
+             * 
+             * @param {type} data
+             * @returns {Array}
+             */
+            cleanData: function (data) {
+                var level = 0;
+                function copy(o) {
+                    var _out, v, _key;
+                    _out = Array.isArray(o) ? [] : {};
+                    for (_key in o) {
+                        v = o[_key];
+                        if (typeof v === "object" && v !== null) {
+                            level++;
+                            _out[_key] = copy(v);
+                            level--;
+                        } else {
+                            if (!level) {
+                                _out[_key] = v;
+                            } else {
+                                if (_key === 'id') {
+                                    _out[_key] = v;
+                                }
+                            }
+                        }
+                    }
+                    return _out;
+                }
+                return copy(data);
+            }
+        };
+    });
+
+}());
