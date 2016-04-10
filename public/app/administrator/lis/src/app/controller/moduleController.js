a8f2f7195c1be145fa7c73db34c61ee017b24fdf
mobiledev <mobiledev@192.168.30.158>
2016-04-10 17:32:37 +0300

    #176 added pdf back, no good news.

--- public/app/administrator/lis/src/app/controller/moduleController.js
+++ public/app/administrator/lis/src/app/controller/moduleController.js
@@ -110,7 +110,26 @@
                     {field: 'moduleCode'},
                     {field: 'duration'},
                     {field: 'trashed'}
-                ]
+                ],
+                enableGridMenu: true,
+                enableSelectAll: true,
+                exporterCsvFilename: 'modules.csv',
+                exporterPdfDefaultStyle: {fontSize: 9},
+                exporterPdfTableStyle: {margin: [30, 30, 30, 30]},
+                exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, italics: true, color: 'red'},
+                exporterPdfHeader: {text: "My Header", style: 'headerStyle'},
+                exporterPdfFooter: function (currentPage, pageCount) {
+                    return {text: currentPage.toString() + ' of ' + pageCount.toString(), style: 'footerStyle'};
+                },
+                exporterPdfCustomFormatter: function (docDefinition) {
+                    docDefinition.styles.headerStyle = {fontSize: 22, bold: true};
+                    docDefinition.styles.footerStyle = {fontSize: 10, bold: true};
+                    return docDefinition;
+                },
+                exporterPdfOrientation: 'portrait',
+                exporterPdfPageSize: 'LETTER',
+                exporterPdfMaxGridWidth: 500,
+                exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location"))
             };
 
             /*
